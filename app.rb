bf88f80fc183ae5571d6239050ec45899559696b
harim1206 <harim1206@gmail.com>
2018-04-03 10:46:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
@@ -8,6 +9,9 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+
+    # binding.pry
 
     erb :results
   end
